6332af353c249f839138d7b2d1598d087d92d5ea
Thomas-emad <tomtom22006@gmail.com>
2024-01-15 19:47:03 +0200

    add more types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -12,8 +12,8 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $types = ['Programming', 'Data Engineering', 'Data Science', 'Technology', 'Self Improvement', 'Writing', 'Machine Learning', 'Productivity', 'Politics', 'Cryptocurrency', 'Psychology', 'History', 'Money', 'Business', 'Coding', 'Software Development', 'Artificial Intelligence (AI)', 'Web Development', 'Android', 'Apple', 'UI', 'UX'];
-        foreach ($types as $type):
+        $types = ['Programming', 'Data Engineering', 'Data Science', 'Technology', 'Self Improvement', 'Writing', 'Machine Learning', 'Productivity', 'Politics', 'Cryptocurrency', 'Psychology', 'History', 'Money', 'Business', 'Coding', 'Software Development', 'Artificial Intelligence (AI)', 'Web Development', 'Android', 'Apple', 'UI', 'UX', 'API', 'Laravel', "React", "Angular", 'Vue', "Node.js", 'C', "C++", "C#", "Python", "Ruby", "Swift", "Kotlin", "Java", "R", "SQL", "NoSQL", "TypeScript"];
+        foreach ($types as $type) :
             \App\Models\Type::create([
                 'name' => $type,
             ]);
